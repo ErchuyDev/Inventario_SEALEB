1a06175ccf8a9de1c7fcdec6adc34a6f5d0934d4
ErchuyDev <erchuy1993@gmail.com>
2025-08-20 21:45:59 -0400

    Configuración de MariaDB y entorno virtual

--- app/config/db.py
+++ app/config/db.py
@@ -28,13 +28,14 @@ POSTGRESQL = {
 MYSQL = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db',
-        'USER': 'root',
-        'PASSWORD': '',
+        'NAME': 'sealeb',
+        'USER': 'sealeb',
+        'PASSWORD': 'valladares',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
-            'sql_mode': 'traditional',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
         }
     }
 }
